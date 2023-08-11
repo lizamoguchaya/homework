037aa64e5b7dd51b3c20abd13d69f03457770593
lizamoguchaya <moguchaya-liza2013@yandex.ru>
2023-08-11 12:50:33 +0300

    cделано все до задания 9, после задания 9 буду сохранять каждую правку, чтобы было видно

---
+++ hw-N2.2.js
@@ -0,0 +1,53 @@
+let a = 10;
+ alert(a);
+
+ let aa = 20;
+ alert(aa)
+
+ let name = 'Brendan Eich';
+ alert(name);
+
+ let x = 10;
+ let up = 2;
+ let sum = x + up;
+ alert(sum);
+
+ let number = 10;
+ let down = 2;
+ let difference = x - up;
+ alert(difference);
+
+ let y = 10;
+ let z = 2;
+ let count = y * z;
+ alert(count);
+
+ let b = 10;
+ let c = 2;
+ let division = 10 / 2; 
+ alert(division);
+
+ let f = 2;
+ let g = 5;
+ let degree = 2 ** 5;
+ alert(degree);
+
+ let ab = 9; 
+ let bc = 2;
+ let mod = 9 % 2;
+ alert(mod);
+
+
+ let num = 1;
+ num += 5;
+ num -= 3;
+ num *= 7;
+ num /= 3;
+ num += 1;
+ num -= 1;
+ alert(num);
+
+ let age = prompt('Сколько вам лет?');
+ alert(age)
+
+ 
